c1c61cf1997ce81867eb66b80f904a365934f00e
olpemila <olli.p.m.laiho@student.jyu.fi>
2024-10-30 14:15:20 +0200

    turhien asioiden poistamista

--- ChatHuone/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ChatHuone.AssemblyInfo.cs
+++ ChatHuone/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ChatHuone.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatHuone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5cb06790b9e217f55d133e78e51085d66a269a22")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatHuone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatHuone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
