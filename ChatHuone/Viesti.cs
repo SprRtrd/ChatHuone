e54c880f5dadad7e033952a22b47d748af7b9ccc
olpemila <olli.p.m.laiho@student.jyu.fi>
2024-11-23 11:24:16 +0200

    luotiin uusi viestiluokka

---
+++ ChatHuone/Viesti.cs
@@ -0,0 +1,8 @@
+namespace ChatHuone;
+
+public class Viesti{
+
+    private int Tyyppi {get; set;} = 1;
+
+    private string Sisalto {get; set;} = "";
+}
--- ChatHuone/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ChatHuone.AssemblyInfo.cs
+++ ChatHuone/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ChatHuone.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatHuone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a2819afc83f9aa724195e3f4a2d9e65cc8487900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1af3e60eda68a8a5aa818938098573c26e53671e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatHuone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatHuone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
