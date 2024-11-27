f8e5463ab85fc785f840f60821fadf1270540f05
olpemila <olli.p.m.laiho@student.jyu.fi>
2024-11-27 13:20:30 +0200

    luotu viesticontrollerluokka

--- ChatHuone/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ChatHuone.AssemblyInfo.cs
+++ ChatHuone/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ChatHuone.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatHuone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8861c608af7e8303e2753de2bacf6a0b90f30f07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+363790fc0a22f9edf8f1791d60b6bc9b16e54483")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatHuone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatHuone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
